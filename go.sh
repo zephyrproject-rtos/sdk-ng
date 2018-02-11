4793d91c9e45b23e0bc469b6ca59df834abe722e
Anas Nashif <anas.nashif@intel.com>
2018-02-10 22:18:07 -0600

    configs: add more architectures, use xtools master
    Update with latest HEAD version of xtools and add new configs for
    additional toolchains, including riscv32 which is experimental.
    
    To use, run:
    
    ./go.sh <arch name>
    
    This will generate the toolchain of choice in build/output
    
    The script does use sudo to install ct-ng in /usr/local/bin.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- go.sh
+++ go.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-TARGET=${1}
+TARGETS=${@}
 
-if [ -z "$TARGET" ]; then
+
+if [ -z "$TARGETS" ]; then
 	echo "Please specify target"
 	exit 1
 fi
+
 REQUIRED_VERSION=1.23.0
+COMMIT="aca85cbb"
 GITDIR=${PWD}
-ct-ng > /dev/null 2>&1 | grep crosstool-ng-${REQUIRED_VERSION}
+
+ct-ng 2>&1 | grep crosstool-ng-${REQUIRED_VERSION} | grep ${COMMIT}
 if [ "$?" == "0" ]; then
 	CTNG_INSTALLED=1
 fi
@@ -36,26 +40,40 @@ if [ "$machine" == "Mac" ]; then
 	cd /Volumes/$ImageName
 fi
 
-if [ -n "$CTNG_INSTALLED" ]; then
-	if [ ! -e "crosstool-ng-${REQUIRED_VERSION}.tar.bz2" ]; then
-		wget http://crosstool-ng.org/download/crosstool-ng/crosstool-ng-${REQUIRED_VERSION}.tar.bz2
+if [ -z "$CTNG_INSTALLED" ]; then
+	if [ ! -d "crosstool-ng" ]; then
+		git clone https://github.com/crosstool-ng/crosstool-ng.git
 	fi
-	rm -rf crosstool-ng-${REQUIRED_VERSION}
-	tar xf crosstool-ng-${REQUIRED_VERSION}.tar.bz2
-
-
-	pushd crosstool-ng-${REQUIRED_VERSION}/
-	patch -p1 < ${GITDIR}/patches/0001-iamcu-support-x86-iamcu-ABIs.patch
+	pushd crosstool-ng
+	git checkout aca85cbb3d9cf0247674464a55246029d5820114
+	if [[ -n $(git status --porcelain) ]]; then
+		patch -p1 < ${GITDIR}/patches/0001-iamcu-support-x86-iamcu-ABIs.patch
+	fi
+	./bootstrap
 	./configure
-	make && make install
+	make && sudo make install
 	popd
+else
+	echo "ctng already installed with correct version"
 fi
-
 mkdir -p build
 cd build
 export CT_PREFIX=`pwd`/output
 mkdir -p ${CT_PREFIX}/sources
-ct-ng clean
-cp ${GITDIR}/configs/${TARGET}.config .config
-ct-ng oldconfig
-ct-ng build
+for t in ${TARGETS}; do
+	if [ ! -f ${GITDIR}/configs/${t}.config ]; then
+		echo "Target configuration does not exist"
+		exit 1
+	fi
+	echo "Building ${t}"
+	mkdir -p build_${t}
+	pushd build_${t}
+
+	ct-ng clean
+	cp ${GITDIR}/configs/${t}.config .config
+	yes "" | ct-ng oldconfig
+	ct-ng build
+
+	popd
+
+done
