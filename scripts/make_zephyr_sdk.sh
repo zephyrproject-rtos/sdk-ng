e33fea1916df745928e25f1eaa744eca296f52c3
Daniel Leung <daniel.leung@intel.com>
2019-08-29 13:32:50 -0700

    xtensa: add config intel_apl_adsp
    This adds the config to build the toolchain for
    the Audio DSP on Intel Apollo Lake SoC.
    
    The overlay archive is from branch sof-gcc8.1,
    commit 2d30f07842f73d9bf0749528b2cdbc71cea1d998 at
    https://github.com/thesofproject/xtensa-overlay
    
    The config has the selections of tools and their versions
    from the Sound Open Firmware project.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- scripts/make_zephyr_sdk.sh
+++ scripts/make_zephyr_sdk.sh
@@ -58,6 +58,7 @@ parse_toolchain_name file_gcc_xtensa xtensa
 parse_toolchain_name file_gcc_riscv64 riscv64
 parse_toolchain_name file_gcc_x86_64 x86_64-zephyr-elf
 parse_toolchain_name file_gcc_xtensa_sample_controller xtensa_sample_controller
+parse_toolchain_name file_gcc_xtensa_intel_apl_adsp xtensa_intel_apl_adsp
 parse_toolchain_name file_hosttools hosttools
 
 # Host tools are non-optional
@@ -133,6 +134,13 @@ fi
 if [ -n "$file_gcc_xtensa_sample_controller" ]; then
   echo "tar -C \$target_sdk_dir -jxf ./$file_gcc_xtensa_sample_controller > /dev/null &" >> $setup
   echo "spinner \$!  \"Installing xtensa sample_controller tools...\"" >> $setup
+  echo "[ \$? -ne 0 ] && echo \"Error(s) encountered during installation.\" && exit 1" >>$setup
+  echo "echo \"\"" >>$setup
+fi
+
+if [ -n "$file_gcc_xtensa_intel_apl_adsp" ]; then
+  echo "tar -C \$target_sdk_dir -jxf ./$file_gcc_xtensa_intel_apl_adsp > /dev/null &" >> $setup
+  echo "spinner \$!  \"Installing xtensa intel_apl_adsp tools...\"" >> $setup
   echo "[ \$? -ne 0 ] && echo \"Error(s) encountered during installation.\" && exit 1" >>$setup
   echo "echo \"\"" >>$setup
 fi
