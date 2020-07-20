fe5e2b4caf622c30c8f0fe6566eba0555412b85c
Kumar Gala <kumar.gala@linaro.org>
2020-07-20 10:01:22 -0500

    make_zephyr_sdk: move to getting machine from argv
    We shouldn't use 'uname -m' since its not the case that we run this
    script on the same host machine type.  So instead just take the machine
    type as an argument when we invoke the script.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- scripts/make_zephyr_sdk.sh
+++ scripts/make_zephyr_sdk.sh
@@ -13,9 +13,14 @@
 
 product_name="zephyr-sdk"
 
+if [ $# != "1" ]; then
+	echo "usage: $0 <MACHINE>"
+	exit 1
+fi
+
 root_dir=$(dirname $0)/..
 sdk_version=$(cat $root_dir/VERSION)
-machine=$(uname -m)
+machine=$1
 arch_list="arm arm64 arc nios2 riscv64 sparc x86_64 xtensa_sample_controller \
            xtensa_intel_apl_adsp xtensa_intel_s1000 xtensa_intel_bdw_adsp \
 	   xtensa_intel_byt_adsp xtensa_nxp_imx_adsp xtensa_nxp_imx8m_adsp"
