f4de2807c78730af01e01dc104ea0fdeeebe6e1d
Kumar Gala <kumar.gala@linaro.org>
2021-07-22 15:07:44 -0500

    go.sh: Rework build script
    Cleanup script and rework things to be more explicit
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- go.sh
+++ go.sh
@@ -17,47 +17,58 @@ GITDIR=${PWD}
 JOBS=$(python -c 'import multiprocessing as mp; print(mp.cpu_count())')
 
 unameOut="$(uname -s)"
-unameMachine="$(uname -m)"
+machine="$(uname -m)"
 case "${unameOut}" in
-    Linux*)     machine=Linux;;
-    Darwin*)    machine=Mac;;
-    CYGWIN*)    machine=Cygwin;;
-    MINGW*)     machine=MinGw;;
-    *)          machine="UNKNOWN:${unameOut}"
+    Linux*)     os=linux;;
+    Darwin*)    os=macos;;
+    CYGWIN*)    os=cygwin;;
+    MINGW*)     os=mingw;;
+    *)          os="UNKNOWN:${unameOut}"
 esac
 
-if [ "$machine" == "Mac" ]; then
+SDK_NG_HOME=${PWD}
+
+if [ "$os" == "macos" ]; then
+	if [ -x "/opt/homebrew/bin/brew" ]; then
+		export HOMEBREW_ROOT="/opt/homebrew"
+	elif [ -x "/usr/local/bin/brew" ]; then
+		export HOMEBREW_ROOT="/usr/local"
+	else
+		echo "No brew install found"
+		exit 1
+	fi
+	export PATH="$PATH:${HOMEBREW_ROOT}/opt/binutils/bin"
+	export CPPFLAGS="-I${HOMEBREW_ROOT}/opt/ncurses/include -I${HOMEBREW_ROOT}/opt/gettext/include"
+	export LDFLAGS="-L${HOMEBREW_ROOT}/opt/ncurses/lib -L${HOMEBREW_ROOT}/opt/gettext/lib"
+
 	ImageName=CrossToolNGNew
 	ImageNameExt=${ImageName}.sparseimage
+	SDK_NG_HOME="/Volumes/${ImageName}"
 	if [ ! -e "$ImageNameExt" ]; then
-	diskutil umount force /Volumes/${ImageName} && true
-	rm -f ${ImageNameExt} && true
-	hdiutil create ${ImageName} -volname ${ImageName} -type SPARSE -size 64g -fs HFSX
+		diskutil umount force ${SDK_NG_HOME} && true
+		rm -f ${ImageNameExt} && true
+		hdiutil create ${ImageName} -volname ${ImageName} -type SPARSE -size 64g -fs HFSX
 	fi
-	if [ ! -d "/Volumes/$ImageName" ]; then
+	if [ ! -d ${SDK_NG_HOME} ]; then
 		hdiutil mount ${ImageNameExt}
 	fi
-	cp -a patches /Volumes/$ImageName
-	cp -a patches-arc64 /Volumes/$ImageName
-	cp -a overlays /Volumes/$ImageName
-	cd /Volumes/$ImageName
 fi
 
-export SDK_NG_HOME=${PWD}
+cp -a ${GITDIR}/patches-arc64 ${SDK_NG_HOME} 2>/dev/null
+cd ${SDK_NG_HOME}
 
 for t in ${TARGETS}; do
 	if [ "${t}" = "tools" ]; then
 		./meta-zephyr-sdk/scripts/meta-zephyr-sdk-clone.sh;
 		./meta-zephyr-sdk/scripts/meta-zephyr-sdk-build.sh tools;
-		mv ./meta-zephyr-sdk/scripts/toolchains/zephyr-sdk-${unameMachine}-hosttools-standalone-0.9.sh .
+		mv ./meta-zephyr-sdk/scripts/toolchains/zephyr-sdk-${machine}-hosttools-standalone-0.9.sh .
 	elif [ "${t}" = "cmake" ]; then
-		tar -jcvf ${t}.${unameMachine}.tar.bz2 -C ${GITDIR} cmake;
+		tar -jcvf ${t}.${machine}.tar.bz2 -C ${GITDIR} cmake
 	fi
 done
 
 if [ ! -d "crosstool-ng" ]; then
 	git clone https://github.com/zephyrproject-rtos/crosstool-ng.git
-	echo "Patching tree"
 	pushd crosstool-ng
 	git checkout ${COMMIT}
 	popd
@@ -72,11 +83,10 @@ if [ ! -e "${SDK_NG_HOME}/bin/ct-ng" ]; then
 fi
 
 CT_NG=${SDK_NG_HOME}/bin/ct-ng
+OUTPUT_DIR=${SDK_NG_HOME}/build/output
 
-mkdir -p build
-cd build
-export OUTPUT_DIR=`pwd`/output
 mkdir -p ${OUTPUT_DIR}/sources
+
 for t in ${TARGETS}; do
 	if [ "${t}" = "tools" ]; then
 		# We handled tools above, so skip it here
@@ -90,33 +100,41 @@ for t in ${TARGETS}; do
 		exit 1
 	fi
 	echo "Building ${t}"
-	mkdir -p build_${t}
-	pushd build_${t}
+	TARGET_BUILD_DIR=${SDK_NG_HOME}/build/build_${t}
+	mkdir -p ${TARGET_BUILD_DIR}
+	pushd ${TARGET_BUILD_DIR}
 
-	build_toolchain=1
-	if [ -n "${build_toolchain}" ]; then
-		case "${t}" in
-			xtensa_*)
-				cp -a ${SDK_NG_HOME}/overlays .
-				export CT_PREFIX=${OUTPUT_DIR}/xtensa/${t#xtensa_}
-				mkdir -p ${CT_PREFIX}
-				;;
-			*)
-				export CT_PREFIX=${OUTPUT_DIR}
-				;;
-		esac
+	case "${t}" in
+		xtensa_*)
+			cp -a ${GITDIR}/overlays ${TARGET_BUILD_DIR}
+			export CT_PREFIX=${OUTPUT_DIR}/xtensa/${t#xtensa_}
+			mkdir -p ${CT_PREFIX}
+			;;
+		*)
+			export CT_PREFIX=${OUTPUT_DIR}
+			;;
+	esac
 
-		${CT_NG} clean
-		${CT_NG} defconfig DEFCONFIG=${GITDIR}/configs/${t}.config
-		${CT_NG} savedefconfig DEFCONFIG=${t}.config
-		${CT_NG} build -j ${JOBS}
-		if [ $? != 0 ]; then
-			exit 1
-		fi
-		rm -rf ${CT_PREFIX}/*/newlib-nano
+	${CT_NG} clean
+	cp ${GITDIR}/configs/${t}.config ${TARGET_BUILD_DIR}/defconfig
+
+	# Disable python support in GDB on MacOS, this isn't currently working.
+	# It builds ok, but the resulting GDB segfaults.
+	if [ "$os" == "macos" ]; then
+		sed -i -e '/CT_GDB_CROSS_PYTHON_BINARY/d' defconfig
+		sed -i -e '/CT_GDB_CROSS_BUILD_NO_PYTHON/d' defconfig
+		sed -i -e '/^CT_CC_GCC_EXTRA_CONFIG_ARRAY=/ s/"$/ --without-zstd"/' defconfig
+		echo "# CT_GDB_CROSS_PYTHON is not set" >> defconfig
 	fi
 
-	popd
-	test -n ${DEBUG_BUILD} || rm -rf  build_${t}
+	${CT_NG} defconfig DEFCONFIG=${TARGET_BUILD_DIR}/defconfig
+	${CT_NG} savedefconfig DEFCONFIG=${TARGET_BUILD_DIR}${t}.config
+	${CT_NG} build -j ${JOBS}
+	if [ $? != 0 ]; then
+		exit 1
+	fi
+	rm -rf ${CT_PREFIX}/*/newlib-nano
 
+	popd
+	rm -fr ${SDK_NG_HOME}/build/build_${t}
 done
