34b88dc6eef463155e2629af77f0612ab65aedf2
Stephanos Ioannidis <root@stephanos.io>
2022-06-26 00:19:42 +0900

    overlays: xtensa_nxp_imx8m_adsp: gdb: Update xtensa-config.c
    This commit updates the `xtensa_tdep` global variable definition in the
    GDB `xtensa-config.c` to use the new type introduced in GDB 12.1.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- overlays/xtensa_nxp_imx8m_adsp/gdb/gdb/xtensa-config.c
+++ overlays/xtensa_nxp_imx8m_adsp/gdb/gdb/xtensa-config.c
@@ -109,7 +109,7 @@ const xtensa_mask_t xtensa_mask37 = { 1, xtensa_submask37 };
 
 
 /* Register map.  */
-xtensa_register_t rmap[] = 
+static xtensa_register_t rmap[] = 
 {
   /*    idx ofs bi sz al targno  flags cp typ group name  */
   XTREG(  0,  0,32, 4, 4,0x0020,0x0006,-2, 9,0x0100,pc,          0,0,0,0,0,0)
@@ -352,9 +352,4 @@ xtensa_register_t rmap[] =
   XTREG_END
 };
 
-
-
-#ifdef XTENSA_CONFIG_INSTANTIATE
-XTENSA_CONFIG_INSTANTIATE(rmap,16)
-#endif
-
+xtensa_gdbarch_tdep xtensa_tdep (rmap);
