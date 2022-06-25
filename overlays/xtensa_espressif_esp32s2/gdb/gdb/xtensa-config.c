2849d324778f1f226a15a5f68fad035b81beaf85
Stephanos Ioannidis <root@stephanos.io>
2022-06-26 00:24:05 +0900

    overlays: xtensa_espressif_esp32s2: gdb: Update xtensa-config.c
    This commit updates the `xtensa_tdep` global variable definition in the
    GDB `xtensa-config.c` to use the new type introduced in GDB 12.1.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- overlays/xtensa_espressif_esp32s2/gdb/gdb/xtensa-config.c
+++ overlays/xtensa_espressif_esp32s2/gdb/gdb/xtensa-config.c
@@ -47,7 +47,7 @@ const xtensa_mask_t xtensa_mask6 = { 1, xtensa_submask6 };
 
 
 /* Register map.  */
-xtensa_register_t rmap[] = 
+static xtensa_register_t rmap[] = 
 {
   /*    idx ofs bi sz al targno  flags cp typ group name  */
   XTREG(  0,  0,32, 4, 4,0x0020,0x0006,-2, 9,0x2100,pc,          0,0,0,0,0,0)
@@ -268,9 +268,4 @@ xtensa_register_t rmap[] =
   XTREG_END
 };
 
-
-
-#ifdef XTENSA_CONFIG_INSTANTIATE
-XTENSA_CONFIG_INSTANTIATE(rmap,12)
-#endif
-
+xtensa_gdbarch_tdep xtensa_tdep (rmap);
