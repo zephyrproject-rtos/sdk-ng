46d091e8fea557f02360dc1e7f479da01eedb94f
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2022-08-31 12:46:41 +0200

    cmake: support loading of host-tool through COMPONENTS
    Support loading of host tools as part of Zephyr-sdk.
    
    This allows users of Zephyr SDK to do:
    `find_package(Zephyr-sdk COMPONENTS HostTools)`
    instead of having to do something like:
    `find_package(Zephyr-sdk COMPONENTS HostTools)`
    `include(${ZEPHYR_SDK}/cmake/zephyr/host-tools.cmake)`
    
    it further improve loading of host-tools as those are only loaded if
    installed.
    Because host tools are now loaded as a find module, then generic CMake
    reporting regarding module loading is used.
    
    This allows us to cleanup Zephyr CMake code as well as place the
    responsibility of checking and loading of host tools into Zephyr SDK.
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

--- cmake/Zephyr-sdkConfig.cmake
+++ cmake/Zephyr-sdkConfig.cmake
@@ -13,6 +13,14 @@ get_filename_component(ZEPHYR_SDK_INSTALL_DIR ${CMAKE_CURRENT_LIST_DIR}/.. ABSOL
 set(ZEPHYR_SDK_INSTALL_DIR ${ZEPHYR_SDK_INSTALL_DIR})
 set(ZEPHYR_TOOLCHAIN_VARIANT zephyr)
 
+# Load additional components. Only components inside Zephyr SDK are allowed.
+set(CURRENT_CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH})
+set(CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
+foreach(component ${Zephyr-sdk_FIND_COMPONENTS})
+  find_package(Zephyr-sdk_${component})
+endforeach()
+set(CMAKE_MODULE_PATH ${CURRENT_CMAKE_MODULE_PATH})
+
 # Those are CMake package parameters.
 set(Zephyr-sdk_FOUND True)
 set(Zephyr-sdk_DIR   ${ZEPHYR_SDK_INSTALL_DIR})
