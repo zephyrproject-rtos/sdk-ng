261eab0393e1f3cd954e67117b2d896a006ff888
Stephanos Ioannidis <root@stephanos.io>
2024-10-24 19:35:56 +0900

    cmake: Add LLVM toolchain support
    This commit adds the support file for the `zephyr-llvm` toolchain
    variant.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

---
+++ cmake/zephyr/llvm/generic.cmake
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: Apache-2.0
+
+set(LLVM_TOOLCHAIN_PATH ${ZEPHYR_SDK_INSTALL_DIR}/llvm)
+
+set(TOOLCHAIN_HOME ${ZEPHYR_SDK_INSTALL_DIR}/llvm/bin)
+
+set(COMPILER clang)
+set(LINKER lld)
+set(BINTOOLS llvm)
+
+set(TOOLCHAIN_HAS_PICOLIBC ON CACHE BOOL "True if toolchain supports picolibc")
