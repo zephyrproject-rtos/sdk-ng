ff0bc4992ef3d926480751ba308f47c83a361b2b
Anas Nashif <anas.nashif@intel.com>
2025-05-04 19:30:25 -0400

    overlays: gdb: fix typo
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- overlays/xtensa_espressif_esp32s2/gdb/gdb/xtensa-config.c
+++ overlays/xtensa_espressif_esp32s2/gdb/gdb/xtensa-config.c
@@ -47,7 +47,7 @@ const xtensa_mask_t xtensa_mask6 = { 1, xtensa_submask6 };
 
 
 /* Register map.  */
-static xtensa_register_t xtena_xtensa_rmap[] =
+static xtensa_register_t xtensa_rmap[] =
 {
   /*    idx ofs bi sz al targno  flags cp typ group name  */
   XTREG(  0,  0,32, 4, 4,0x0020,0x0006,-2, 9,0x2100,pc,          0,0,0,0,0,0)
@@ -268,4 +268,4 @@ static xtensa_register_t xtena_xtensa_rmap[] =
   XTREG_END
 };
 
-xtensa_gdbarch_tdep xtensa_tdep (xtena_xtensa_rmap);
+xtensa_gdbarch_tdep xtensa_tdep (xtensa_rmap);
