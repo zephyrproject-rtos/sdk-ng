5570405dc7b594224654a629b355d8f7348003f2
Anas Nashif <anas.nashif@intel.com>
2025-05-04 18:23:46 -0400

    overlays: gdb: rename rmap -> xtensa_rmap
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- overlays/xtensa_nxp_rt600_adsp/gdb/gdb/xtensa-config.c
+++ overlays/xtensa_nxp_rt600_adsp/gdb/gdb/xtensa-config.c
@@ -113,7 +113,7 @@ const xtensa_mask_t xtensa_mask39 = { 2, xtensa_submask39 };
 
 
 /* Register map.  */
-xtensa_register_t rmap[] = 
+xtensa_register_t xtensa_rmap[] =
 {
   /*    idx ofs bi sz al targno  flags cp typ group name  */
   XTREG(  0,  0,32, 4, 4,0x0020,0x0006,-2, 9,0x2100,pc,          0,0,0,0,0,0)
@@ -415,7 +415,7 @@ xtensa_register_t rmap[] =
 
 
 #ifdef XTENSA_CONFIG_INSTANTIATE
-XTENSA_CONFIG_INSTANTIATE(rmap,16)
+XTENSA_CONFIG_INSTANTIATE(xtensa_rmap,16)
 #endif
 
-xtensa_gdbarch_tdep xtensa_tdep (rmap);
+xtensa_gdbarch_tdep xtensa_tdep (xtensa_rmap);
