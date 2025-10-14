8eda11eb39a94d94032635774b08ad3a8c544dd2
Stephanos Ioannidis <root@stephanos.io>
2025-10-14 20:34:36 +0900

    cmake: Add macOS and Windows host tool support
    This commit adds the Zephyr build system support for the host tools
    included in macOS and Windows Zephyr SDK distributions.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- cmake/zephyr/host-tools.cmake
+++ cmake/zephyr/host-tools.cmake
@@ -1,17 +1,41 @@
 # SPDX-License-Identifier: Apache-2.0
 
 cmake_host_system_information(RESULT TOOLCHAIN_ARCH QUERY OS_PLATFORM)
-set(HOST_TOOLS_HOME ${ZEPHYR_SDK_INSTALL_DIR}/hosttools/sysroots/${TOOLCHAIN_ARCH}-pokysdk-linux)
 
-# Path used for searching by the find_*() functions, with appropriate
-# suffixes added. Ensures that the SDK's host tools will be found when
-# we call, e.g. find_program(QEMU qemu-system-x86)
-list(APPEND CMAKE_PREFIX_PATH ${HOST_TOOLS_HOME}/usr)
+if(${CMAKE_HOST_SYSTEM_NAME} STREQUAL Linux)
+  # For Linux SDK, host tool components are built using Yocto and placed into
+  # the Poky sysroot.  The host tool directory layout follows the standard UNIX
+  # filesystem hierarchy.
+  set(HOST_TOOLS_HOME ${ZEPHYR_SDK_INSTALL_DIR}/hosttools/sysroots/${TOOLCHAIN_ARCH}-pokysdk-linux)
 
-# TODO: Use find_* somehow for these as well?
-set_ifndef(QEMU_BIOS            ${HOST_TOOLS_HOME}/usr/share/qemu)
-if (WIN32)
-    set_ifndef(OPENOCD_DEFAULT_PATH $ENV{ProgramFiles}/OpenOCD/share/openocd/scripts)
-else ()
-    set_ifndef(OPENOCD_DEFAULT_PATH ${HOST_TOOLS_HOME}/usr/share/openocd/scripts)
-endif ()
+  # Path used for searching by the find_*() functions, with appropriate
+  # suffixes added. Ensures that the SDK's host tools will be found when
+  # we call, e.g. find_program(QEMU qemu-system-x86)
+  list(APPEND CMAKE_PREFIX_PATH ${HOST_TOOLS_HOME}/usr)
+
+  set_ifndef(QEMU_BIOS            ${HOST_TOOLS_HOME}/usr/share/qemu)
+  set_ifndef(OPENOCD_DEFAULT_PATH ${HOST_TOOLS_HOME}/usr/share/openocd/scripts)
+elseif(${CMAKE_HOST_SYSTEM_NAME} STREQUAL Darwin)
+  # For macOS SDK, each host tool component is placed in its own directory under
+  # the 'opt' directory, with all executable binaries symlinked to 'usr/bin'
+  # directory.
+  set(HOST_TOOLS_HOME ${ZEPHYR_SDK_INSTALL_DIR}/hosttools)
+
+  # Make executable symlinks under 'usr/bin' directory discoverable by the
+  # find_*() functions.
+  list(APPEND CMAKE_PREFIX_PATH ${HOST_TOOLS_HOME}/usr)
+
+  set_ifndef(QEMU_BIOS            ${HOST_TOOLS_HOME}/opt/qemu/share/qemu)
+  set_ifndef(OPENOCD_DEFAULT_PATH ${HOST_TOOLS_HOME}/opt/openocd/scripts)
+elseif(${CMAKE_HOST_SYSTEM_NAME} STREQUAL Windows)
+  # For Windows SDK, each host tool component is placed in its own directory
+  # under the host tools home directory.
+  set(HOST_TOOLS_HOME ${ZEPHYR_SDK_INSTALL_DIR}/hosttools)
+
+  # Add host tool component binary directories to the prefix paths for
+  # discovery by the find_program() function.
+  list(APPEND CMAKE_PREFIX_PATH ${HOST_TOOLS_HOME}/qemu)
+
+  set_ifndef(QEMU_BIOS            ${HOST_TOOLS_HOME}/qemu/share)
+  set_ifndef(OPENOCD_DEFAULT_PATH ${HOST_TOOLS_HOME}/openocd/scripts)
+endif()
